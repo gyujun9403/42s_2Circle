4a5632d934305cb3a7af0c7425e1d9081c32a362
gyujun9403 <gujun112@naver.com>
2021-08-18 17:58:19 +0900

    pipex doing...

--- pipex/main.c
+++ pipex/main.c
@@ -2,50 +2,22 @@
 
 #include <stdio.h>
 
-int main()
+int	check_access(char *file_name)
 {
-	pid_t pid[2];
-	pid_t ret[2];
-
-	pid[0] = fork();
-	if (pid[0] == -1)
+	if (access(file_name, F_OK) == -1)
+	{
+		strerror
 		return (-1);
-	else if (pid[0] == 0)
-	{
-		usleep(200);
-		printf("this is child.1\n");
-		usleep(200);
-		printf("this is child.1\n");
-		usleep(200);
-		printf("this is child.1\n");
-		usleep(200);
-		printf("this is child.1\n");
 	}
-	else
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+		;
+	else if (ac > 3)
 	{
-		printf("%d child has born.\n", pid[0]);
-		pid[1] = fork();
-		if (pid[1] == -1)
-			return (-1);
-		else if (pid[1] == 0)
-		{
-			usleep(200);
-			printf("this is child.2\n");
-			usleep(200);
-			printf("this is child.2\n");
-			usleep(200);
-			printf("this is child.2\n");
-			usleep(200);
-			printf("this is child.2\n");
-		}
-		else
-		{
-			printf("%d child has born.\n", pid[1]);
-			ret[0] = wait(NULL);
-			printf("%d child has been retrieved.\n", ret[0]);
-			ret[1] = wait(NULL);
-			printf("%d child has been retrieved.\n", ret[1]);
-		}
+
 	}
 	return 0;
 }
