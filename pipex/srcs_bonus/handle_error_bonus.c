ecc55de414fb24d393eb241f7c640e9493f0cf76
gyujun9403 <gujun112@naver.com>
2021-09-26 14:05:04 +0900

    pipex final 2.3ver return to 'bash' again

--- pipex/srcs_bonus/handle_error_bonus.c
+++ pipex/srcs_bonus/handle_error_bonus.c
@@ -6,7 +6,7 @@
 /*   By: gyeon <gyeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 13:38:19 by gyeon             #+#    #+#             */
-/*   Updated: 2021/09/25 14:59:41 by gyeon            ###   ########.fr       */
+/*   Updated: 2021/09/26 14:03:16 by gyeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ void	prt_command_not_found(char *str)
 	int		str_len;
 
 	str_len = ft_strlen(str);
-	temp_str = ft_calloc(str_len + 29, sizeof(char));
-	ft_strlcat(temp_str, "gyeon: ", 8);
-	ft_strlcat(temp_str, str, 8 + str_len);
-	ft_strlcat(temp_str, ": command not found\n", 29 + str_len);
+	temp_str = ft_calloc(str_len + 28, sizeof(char));
+	ft_strlcat(temp_str, "bash: ", 7);
+	ft_strlcat(temp_str, str, 7 + str_len);
+	ft_strlcat(temp_str, ": command not found\n", 28 + str_len);
 	ft_putstr_fd(temp_str, STDERR_FILENO);
 	free(temp_str);
 }
