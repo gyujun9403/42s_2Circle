74003e654ce44ae55cf361473aa53cd9ce0140bf
Gyujun Yeon <gyeon@c8r7s2.42seoul.kr>
2021-09-28 18:21:51 +0900

    set files for so_long

---
+++ so_long/header/so_long.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyeon <gyeon@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/28 14:26:29 by gyeon             #+#    #+#             */
+/*   Updated: 2021/09/28 15:18:52 by gyeon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <fcntl.h> //open, 
+# include <unistd.h> //close, read, write
+# include <stdio.h> //printf, perror
+# include <stdlib.h> //malloc, free, exit
+# include <string.h> //strerror
+# include "mlx.h"
+
+#endif
