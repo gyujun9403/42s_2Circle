68e73ccbca5cc779d7744b3e66673425ef735c3a
Gyujun Yeon <gyeon@c8r7s2.42seoul.kr>
2021-09-30 19:05:49 +0900

    so_long testin...

--- so_long/srcs/main.c
+++ so_long/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: gyeon <gyeon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 14:33:23 by gyeon             #+#    #+#             */
-/*   Updated: 2021/09/30 15:01:11 by gyeon            ###   ########.fr       */
+/*   Updated: 2021/09/30 18:15:10 by gyeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,39 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
+void	my_mlx_rec(void *mlx, void	*mlx_win, int st_x, int st_y, int fin_x, int fin_y)
+{
+	int		i;
+	int		backup_st_x;
+	static t_data	img;
+
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	int color = 0x500073;
+	i = 0;
+	backup_st_x = st_x;
+	while (st_y < fin_y)
+	{
+		while (st_x < fin_x)
+		{
+			my_mlx_pixel_put(&img, st_x, st_y, color);
+			mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+			st_x++;
+		}
+		st_y++;
+		st_x = backup_st_x;
+	}
+}
+
 int main()
 {
 	void	*mlx;
 	void	*mlx_win;
-	t_data	img;
+	
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello MLX!");
-	img.img = mlx_new_image(mlx, 50, 50);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	int color = 0x00AA00;
-	my_mlx_pixel_put(&img, 40, 40, color);
-	my_mlx_pixel_put(&img, 41, 40, color);
-	my_mlx_pixel_put(&img, 40, 41, color);
-	my_mlx_pixel_put(&img, 41, 41, color);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	my_mlx_rec(mlx, mlx_win, 100, 200, 200, 300);
 	mlx_loop(mlx);
 	return (0);
 }
